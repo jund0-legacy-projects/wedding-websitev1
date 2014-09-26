33fa128f18765a6e6ddb64afb4f618b2c533ef94
Fredo Picazo <apicazojr@gmail.com>
2014-09-26 08:15:16 -0700

    updated page for skrollr

--- js/dist.js
+++ js/dist.js
@@ -8,4 +8,11 @@ $(function(){
 	console.log(viewportHeight);
 	var wh = $(window).height();
 	jQuery('#main-landing').css({ 'height' : wh });
+
+	var s = skrollr.init({
+                render: function(data) { 
+                    $('#offset').text(data.curTop);
+                },
+                forceHeight : false
+            });
 });
