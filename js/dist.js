fc7df8d69ead859dfadea92e533de4d0b349cab9
Fredo Picazo <apicazojr@gmail.com>
2014-09-25 17:06:07 -0700

    uploading local files to github

---
+++ js/dist.js
@@ -0,0 +1,11 @@
+$(function(){
+	/*var browserWindow = {
+					height : $(window).height(), 
+					width : $(window).width() 
+				}	
+	/*LANDING IMAGE*/
+	var viewportHeight = $(window).height();
+	console.log(viewportHeight);
+	var wh = $(window).height();
+	jQuery('#main-landing').css({ 'height' : wh });
+});
